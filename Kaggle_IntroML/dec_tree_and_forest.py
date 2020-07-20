10da6cf3285eddd63c63f5ebb8550880ac0552c6
Albert Machado <machado.albeiro@gmail.com>
2020-07-20 17:50:47 -0500

    ordering

--- Kaggle_IntroML/dec_tree_and_forest.py
+++ Kaggle_IntroML/dec_tree_and_forest.py
@@ -5,15 +5,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 
+################################ Basic Data Exploration ################################
+
 # Path of the file to read
 iowa_file_path = '../input/home-data-for-ml-course/train.csv'
 
 home_data = pd.read_csv(iowa_file_path)
 # Create target object and call it y
 y = home_data.SalePrice
+
+################################ First model ################################
+
 # Create X
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 X = home_data[features]
+
+################################ Model Validation ################################
 
 # Split into validation and training data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
@@ -27,6 +34,8 @@ iowa_model.fit(train_X, train_y)
 val_predictions = iowa_model.predict(val_X)
 val_mae = mean_absolute_error(val_predictions, val_y)
 print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+################################ Underfitting and overfitting ################################
 
 # Using best value for max_leaf_nodes
 iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
